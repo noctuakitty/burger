42046b4ee18a7029c198139d182d502900e72562
Kilea Castillo <kileaw@gmail.com>
2020-05-31 14:40:27 -0600

    can't find the error

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,22 +1,20 @@
-$(document).ready(function() {
-$("#submitButton").on("click", function (event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-    var newBurger = {
-        burger: $("#inputDefault").val().trim()
-    };
-    console.log(newBurger);
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-    }).then(
-        function () {
-            location.reload();
-        }
-    );
+$(document).ready(function () {
+    $("#submitButton").on("click", function (event) {
+        event.preventDefault();
+        var newBurger = {
+            burger: $("#inputDefault").val().trim()
+        };
+        console.log(newBurger);
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
 });
-
 $(document, ".devourBurger").on("click", function (event) {
     event.preventDefault();
     var id = $(event.target).data("id");
@@ -29,9 +27,7 @@ $(document, ".devourBurger").on("click", function (event) {
         data: devouredBurg
     }).then(
         function () {
-            // Reload the page to get the updated list
             location.reload();
         }
     );
-});
 });
