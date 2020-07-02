30197a26d7f7eb1bface386cd57f2d4765cbf3c2
Kilea Castillo <kileaw@gmail.com>
2020-07-01 19:26:43 -0600

    fixing routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,10 @@
 var Burger = require("../models/burgers.js");
 
 module.exports = function(app) {
+  app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "/../public/index.html"))
+  });
+  
   app.get("/api/all", function(req, res) {
     Burger.findAll({}).then(function(result){
       console.log(result)
